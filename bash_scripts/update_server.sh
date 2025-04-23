aa241ae7c95828daca111286bb542475da6c63fb
Adam Simba <adamsimba29@yahoo.com>
2025-04-23 15:48:55 +0300

    Addition of the bash automation scripts

---
+++ bash_scripts/update_server.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+UPDATE_LOG_FILE="/var/log/update.log"
+REPO_DIR="https://github.com/simba2910/assignment1.git"
+TIMESTAMP=$(date '+%Y-%m-%d %H:%M:%S')
+
+echo "[$TIMESTAMP] Updating system and API..." >> $UPDATE_LOG_FILE
+apt update && apt upgrade -y >> $UPDATE_LOG_FILE 2>&1
+
+cd $REPO_DIR
+if git pull origin master>> $UPDATE_LOG_FILE 2>&1; then
+  echo "Pulled latest code from GitHub." >> $UPDATE_LOG_FILE
+  systemctl restart nginx
+  echo "Nginx restarted." >> $UPDATE_LOG_FILE
+else
+  echo "ERROR: Git pull failed. Nginx not restarted." >> $UPDATE_LOG_FILE
+  exit 1
+fi
